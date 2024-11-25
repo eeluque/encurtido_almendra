d2f43adc020f2fdc423139223983d075dc13242e
eeluque <eduardoluque08@gmail.com>
2024-11-25 08:27:10 -0600

    making layout changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (<>
-        <div className="bg-slate-500">
+        <div className="bg-slate-500 w-full h-28 absolute bottom-0">
             <h1>And this is going to be the footer</h1>
         </div>
     </>)
---
+++ src/app/components/Product_Tile.tsx
@@ -0,0 +1,6 @@
+export default function Product_Tile() {
+    return (
+        <div className="border-2 w-4/12 h-2/6 bg-cyan-600">
+            <p>I'll be a product tile someday</p>
+        </div>)
+}
